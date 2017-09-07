8880d62f76e95121bd75e4a481e521401706231f
Tobias <thale.android@gmail.com>
2017-09-07 08:42:52 +0100

    update number and email

--- mail/my_mail.js
+++ mail/my_mail.js
@@ -2,20 +2,20 @@
 sendForm = function(){
   // event.preventDefault
 
-  // dataHash = {
-  //   'name' : $('#name').val(),
-  //   '_replyto' : $('#email').val(),
-  //   'phone' : $('#phone').val(),
-  //   'message' : $('#message').val()
-  //     }
+  dataHash = {
+    'name' : $('#name').val(),
+    '_replyto' : $('#email').val(),
+    'phone' : $('#phone').val(),
+    'message' : $('#message').val()
+      }
 
-  // $.ajax({
-  //   url: 'https://formspree.io/thale@gmail.com',
-  //   method: 'POST',
-  //   dataType: 'json',
-  //   data: dataHash
-  // }).always(function(){
-    // console.log('done')
+  $.ajax({
+    url: 'https://formspree.io/edward08w@hotmail.co.uk',
+    method: 'POST',
+    dataType: 'json',
+    data: dataHash
+  }).always(function(){
+    console.log('done')
     $('#success').html("<div class='alert alert-success'>");
     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
         .append("</button>");
@@ -24,13 +24,13 @@ sendForm = function(){
     $('#success > .alert-success')
         .append('</div>');
     $('form')[0].reset()
-  // })
+  })
 }
 
 
 $(function(){
   $('form').on('submit', function(event){
-    // event.preventDefault()
+    event.preventDefault()
     sendForm()
   })
 })
